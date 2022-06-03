bc2e7cac4973989e17511344a4c5c9e5cbc4f016
Tulench1k <nickdobrov@yandex.ru>
2022-06-04 01:00:16 +0300

    fit: Созданы контроллеры для пользователей и фильмов

---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/Validator.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import ru.yandex.practicum.filmorate.exception.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+public class Validator {
+    public static void validate(Film film) throws FilmValidationException {
+        if (film.getName().isEmpty()) {
+            throw new EmptyNameException();
+        } else if (film.getDescription().length() > 200) {
+            throw new TooLongDescriptionException();
+        } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new IncorrectDateException();
+        } else if (film.getDuration() < 0) {
+            throw new NegativeDurationException();
+        }
+    }
+
+    public static void validate(User user) throws UserValidationException {
+        if (user.getLogin().isBlank()) {
+            throw new BlankLoginException();
+        } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new FutureDateException();
+        }
+    }
+}
