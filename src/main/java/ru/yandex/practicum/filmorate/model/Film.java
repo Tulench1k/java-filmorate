2d273995b63ffa1fff764fcb2698dd93f7fb7d41
Nikolai Dobrov <84470592+Tulench1k@users.noreply.github.com>
2022-06-04 23:21:53 +0300

    Update Film.java
    fix: Исправлены аннотации

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,8 +8,15 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private int id;
-    @NonNull private final String name;
-    @NonNull private final String description;
-    @NonNull private final LocalDate releaseDate;
+    
+    @NonNull 
+    private final String name;
+    
+    @NonNull 
+    private final String description;
+    
+    @NonNull
+    private final LocalDate releaseDate;
+    
     private final int duration;
 }
